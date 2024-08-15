33abc28186d365a0bd12f84e0ae3ec7c69bc9910
Yaroslav <yaroslavpavlenko2@gmail.com>
2024-08-15 23:52:19 +0700

    hotfix

--- DESC_Education/Users/serializers.py
+++ DESC_Education/Users/serializers.py
@@ -16,7 +16,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
 
 class EmptySerializer(serializers.Serializer):
-    pass
+    email = serializers.CharField()
 
 
 class LoginSerializer(serializers.Serializer):
