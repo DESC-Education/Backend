850a560226f9673cb55373ad1bd37fce85bdf565
Yaroslav <yaroslavpavlenko2@gmail.com>
2024-08-16 01:58:28 +0700

    hotfix

--- DESC_Education/Users/urls.py
+++ DESC_Education/Users/urls.py
@@ -18,14 +18,14 @@ from Users.views import (
 urlpatterns = [
 
     # path('admin/', admin.site.urls)
-    path('login/', LoginView.as_view(), name='login'),
-    path('registration/', RegistrationView.as_view(), name='registration'),
-    path('token_refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-    path('registration/verify/', VerifyRegistrationView.as_view(), name='verify_registration'),
-    path('auth/', AuthView.as_view(), name='auth'),
-    path('send_verify_code/', SendVerifyCodeView.as_view(), name='send_verify_code'),
-    path('change_password/', ChangePasswordView.as_view(), name='change_password'),
-    path('change_email/', ChangeEmailView.as_view(), name='change_email'),
+    path('login', LoginView.as_view(), name='login'),
+    path('registration', RegistrationView.as_view(), name='registration'),
+    path('token_refresh', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('registration/verify', VerifyRegistrationView.as_view(), name='verify_registration'),
+    path('auth', AuthView.as_view(), name='auth'),
+    path('send_verify_code', SendVerifyCodeView.as_view(), name='send_verify_code'),
+    path('change_password', ChangePasswordView.as_view(), name='change_password'),
+    path('change_email', ChangeEmailView.as_view(), name='change_email'),
 
 
     # path('', HelloView.as_view(), name='hello'),
