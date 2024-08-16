a50ba1b1305290d98040e4bd24f6c3852e6a8f87
Yaroslav <yaroslavpavlenko2@gmail.com>
2024-08-16 19:08:30 +0700

    models

--- DESC_Education/Profiles/models.py
+++ DESC_Education/Profiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Users.models import CustomUser
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4,
+                          editable=False,
+                          unique=True)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+
