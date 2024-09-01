d3299080029c23a27df4ed9f4b6d4cf62809e475
Yaroslav <yaroslavpavlenko2@gmail.com>
2024-09-01 20:43:05 +0700

    added TaskCategoryListView

--- DESC_Education/Tasks/models.py
+++ DESC_Education/Tasks/models.py
@@ -11,6 +11,7 @@ from django.apps import apps
 
 class TaskCategory(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
     name = models.CharField(max_length=100)
 
     def __str__(self):
