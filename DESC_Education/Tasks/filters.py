724cfaf5157eb57eb6216ac61f011c7b359f7def
Yaroslav <yaroslavpavlenko2@gmail.com>
2024-09-10 21:11:57 +0700

    added ordering

--- DESC_Education/Tasks/filters.py
+++ DESC_Education/Tasks/filters.py
@@ -1,3 +1,4 @@
+import attrs
 from django_filters import rest_framework as drf_filters
 from Tasks.models import Task, Filter, TaskCategory
 
@@ -5,6 +6,19 @@ from Tasks.models import Task, Filter, TaskCategory
 class TaskFilter(drf_filters.FilterSet):
     filters = drf_filters.ModelMultipleChoiceFilter(queryset=Filter.objects.all(), field_name='filters')
     category = drf_filters.ModelChoiceFilter(queryset=TaskCategory.objects.all(), field_name='category')
+
+    ordering = drf_filters.OrderingFilter(
+        fields=(
+            ('created_at', 'createdAt'),
+        ),
+
+        # labels do not need to retain order
+        field_labels={
+            'created_at': 'По дате добавления',
+        }
+
+    )
+
 
     class Meta:
         model = Task
