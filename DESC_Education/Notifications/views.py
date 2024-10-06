c75c19a734ac2bf2049ff49afb75fe7ccbf9c6c2
Yaroslav <yaroslavpavlenko2@gmail.com>
2024-10-06 13:26:27 +0700

    fix ws tests

--- DESC_Education/Notifications/views.py
+++ DESC_Education/Notifications/views.py
@@ -6,6 +6,9 @@ from rest_framework import generics
 from Settings.permissions import IsAuthenticatedAndVerified
 from Notifications.models import Notification
 from Notifications.serializers import NotificationSerializer
+from rest_framework import status
+from django.utils import timezone
+from drf_spectacular.utils import extend_schema
 
 
 # Create your views here.
@@ -22,8 +25,13 @@ class NotificationListView(generics.ListAPIView):
     serializer_class = NotificationSerializer
 
     def get_queryset(self):
-        return Notification.objects.filter(user=self.request.user)
+        return Notification.objects.filter(user=self.request.user,
+                                           created_at__gte=(timezone.now() - timezone.timedelta(days=7)))
 
+    @extend_schema(
+        tags=["Notifications"],
+        summary="Получение списка уведомлений",
+    )
     def get(self, *args, **kwargs):
         return super().get(*args, **kwargs)
 
@@ -34,7 +42,33 @@ class EventsUser(EventsViewSet):
     def get_queryset(self):
         return Notification.objects.filter(user=self.request.user)
 
+    @extend_schema(
+        tags=["Notifications"],
+        summary="Подключение к sse уведомлений",
+    )
     def list(self, request):
         user = request.user
         self.channels = [f"user-{user.id}"]
         return super().list(request)
+
+
+class ReadNotificationView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticatedAndVerified]
+
+    def get_object(self) -> Notification:
+        instance = generics.get_object_or_404(Notification, pk=self.kwargs.get('pk'))
+        if self.request.user != instance.user:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return instance
+
+    @extend_schema(
+        tags=["Notifications"],
+        summary="Отметка о прочтении сообщения",
+    )
+    def get(self, request, pk, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_read = True
+        instance.save()
+        serializer = NotificationSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
