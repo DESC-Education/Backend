6faddcc645b19e6ec3546b46a99b15b55e860d3f
Yaroslav <yaroslavpavlenko2@gmail.com>
2024-10-21 22:14:56 +0300

    added StatisticsUser

--- DESC_Education/Admins/urls.py
+++ DESC_Education/Admins/urls.py
@@ -3,7 +3,8 @@ from Admins.views import (
     AdminProfileVerifyRequestListView,
     AdminProfileVerifyRequestDetailView,
     AdminCustomUserListView,
-    AdminCustomUserDetailView
+    AdminCustomUserDetailView,
+    StatisticsUserView
 
 )
 
@@ -13,6 +14,9 @@ urlpatterns = [
     path('profile/requests', AdminProfileVerifyRequestListView.as_view(), name='admin_v_request_list'),
     path('profile/request/<uuid:pk>', AdminProfileVerifyRequestDetailView.as_view(), name='admin_v_request_detail'),
     path('users/', AdminCustomUserListView.as_view(), name='admin_user_list'),
-    path('user/<uuid:pk>', AdminCustomUserDetailView.as_view(), name='admin_user_detail')
+    path('user/<uuid:pk>', AdminCustomUserDetailView.as_view(), name='admin_user_detail'),
+
+
+    path('stats/users', StatisticsUserView.as_view(), name='stats_users'),
 
 ]
