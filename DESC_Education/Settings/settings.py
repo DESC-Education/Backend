a19d3150267e0d840f8e89967f82ec7f2d91e2f1
Yaroslav <108934904+YarikATM@users.noreply.github.com>
2025-04-24 20:09:48 +0700

    Update settings.py

--- DESC_Education/Settings/settings.py
+++ DESC_Education/Settings/settings.py
@@ -257,8 +257,8 @@ ADMINS = (
     # ('admin', 'yaroslavpavlenko2@gmail.com'),
 )
 
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
+EMAIL_HOST = 'mail.hosting.reg.ru'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = config.EMAIL_USER.get_secret_value()
 EMAIL_HOST_PASSWORD = config.EMAIL_PASSWORD.get_secret_value()
 EMAIL_USE_SSL = False
