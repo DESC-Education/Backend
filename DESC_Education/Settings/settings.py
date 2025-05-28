cc5d51434064feabf02e49a87219baea84daacd8
Yaroslav <108934904+YarikATM@users.noreply.github.com>
2025-05-28 18:50:55 +0700

    Update settings.py

--- DESC_Education/Settings/settings.py
+++ DESC_Education/Settings/settings.py
@@ -257,11 +257,11 @@ ADMINS = (
     # ('admin', 'yaroslavpavlenko2@gmail.com'),
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.hosting.reg.ru'
-EMAIL_PORT = 25
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
 EMAIL_HOST_USER = config.EMAIL_USER.get_secret_value()
 EMAIL_HOST_PASSWORD = config.EMAIL_PASSWORD.get_secret_value()
+EMAIL_USE_SSL = True
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
